87add2e200412c27b14682a85b5469c4a65920bf
ogruca <ogruca@users.noreply.github.com>
2018-03-01 16:35:12 +1100

    Amended input to raw_input

--- python/SayMyName.py
+++ python/SayMyName.py
@@ -1,8 +1,8 @@
-#SayMyName.py - prints a screen full of the user's name
-
-# Ask the user for their name, un comment this to use this section rather than turtle window above
-name = input("What is your name?")
-
-# Print their name 100 times
+# This program will take your name and print it 100 times
+# This section captures the raw input "name and stores it in the variable "name"
+name = raw_input("What is your name?\n ")
+# The print statement below is for checking that the variable is populated
+print name
+# This for loop will print the variable "name" 100 times
 for x in range(100):
-	print 'name'
+	print name
