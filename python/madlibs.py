bd0dff3d16897c2fb18d3c8bbdfd4dcc38b339d3
ogruca <ogruca@users.noreply.github.com>
2018-03-02 21:28:43 +1100

    Delete madlibs.py

--- python/madlibs.py
+++
@@ -1,7 +0,0 @@
-# This program will take an adjective, noun and past tense verb store them in three different variables then print them out in a sentence.
-adjective = raw_input("Please enter an adjective: ")
-noun = raw_input("Please enter a noun: ")
-verb = raw_input("Please enter a verb ending in ed: ")
-animal = raw_input("Please enter an animal: ")
-print("Your Mindlib:")
-print("The", adjective, noun, verb, "over the lazy brown", animal)
