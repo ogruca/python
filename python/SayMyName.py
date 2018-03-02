340aaa7c750c50e1ed7bae3451fa792ddb444116
ogruca <ogruca@users.noreply.github.com>
2018-03-02 21:27:57 +1100

    Delete SayMyName.py

--- python/SayMyName.py
+++
@@ -1,9 +0,0 @@
-# This program will take your name and print it 100 times
-# This section captures the raw input "name and stores it in the variable "name"
-name = raw_input("What is your name?\n ")
-# The print statement below is for checking that the variable is populated
-print name
-# This for loop will print the variable "name" 100 times
-for x in range(100):
-	print name
-# Added extra line from github
