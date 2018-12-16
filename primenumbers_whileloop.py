3eac7f5223d8c3eaca909157b9819557ecc95b45
ogruca <ogruca@users.noreply.github.com>
2018-12-16 13:58:38 +1100

    added prime numbers

---
+++ primenumbers_whileloop.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+
+sum = 0
+add = 2
+while add != 0:
+    print('The current sum is:',sum)
+    print('How much would you like to add?', end='')
+    raw_add = raw_input('(Type 0 to end program): ')
+    add = int(raw_add)
+    sum = sum + add
