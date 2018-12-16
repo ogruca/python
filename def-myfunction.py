660d775a0e582daf64adc9ff6aca97555fd89c10
ogruca <ogruca@users.noreply.github.com>
2018-12-17 00:44:44 +1100

    updated

--- def-myfunction.py
+++ def-myfunction.py
@@ -1,4 +1,4 @@
-
+# the function exp is defined below
 def exp(x,y):
 	z = x**y
 	print(z)
@@ -6,4 +6,5 @@ def exp(x,y):
 base = input('Please enter the base value: ')
 exponent = input('Please enter the exponent value: ')
 
+#the function exp is called
 exp(base,exponent)
