5cf47d918f1c8cb20405db0db550483e96eea9b6
ogruca <ogruca@users.noreply.github.com>
2018-12-16 16:08:03 +1100

    update

--- lists-for-loop.py
+++ lists-for-loop.py
@@ -1,4 +1,7 @@
 names = ['Ben', 'Sally', 'Amy', 'George', 'Randy']
 
+x = len(names)
+print('There are', x, 'items in the list')
+
 for x in names:
-   print(x)
+    print(x)
---
+++ lists-for-loop2.py
@@ -0,0 +1,6 @@
+"this wil print the list of names below with their index position starting from index 0"
+
+names = ["Ben", "Sally", "Amy", "George", "Randy", "Alice"]
+
+for x in range(0, len(names)):
+    print(names[x], 'is found at index:', x)
